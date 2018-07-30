d459b421b621030b998e3c8816fb48b0a2e62b46
Daniel MacKay <mackay2588@gmail.com>
2018-07-30 13:40:53 -0500

    gen div, counter, swap button work

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,55 @@
 /* jshint esversion: 6*/
 
-console.log('js');
+$(document).ready(readyNow);
+
+function readyNow(){
+    $('#generateBtn').on('click', appendDiv);
+
+    $(document).on('click','.swapBtn',changeStyle);
+}
+
+//variable to hold counter
+let counter = 0;
+
+function appendDiv(){
+    console.log('in appendDiv');
+
+
+    //increment counter
+    counter++;
+
+    //create div to append
+    let divToAppend = $('<div class="appendedDiv red" ></div>');
+    //create p to append to div
+    let pToAppend = $('<p></p>');
+
+    //append counter to p
+    $(pToAppend).html(counter);
+
+    //append p to div
+    divToAppend.append(pToAppend);
+
+    //create buttons to append to div
+    let swapBtn = $('<button id="swapBtn'+ counter + '" class="swapBtn" >Swap</button>');
+    let deleteBtn = $('<button id="deleteBtn' + counter + '" class="deleteBtn" ></button');
+
+    //append string delete to deletBtn html
+    $(deleteBtn).html('Delete');
+
+    //append buttons to div
+    divToAppend.append(swapBtn);
+    divToAppend.append(deleteBtn);
+
+    $('#appendContainer').append(divToAppend);
+
+}
+
+
+function changeStyle(){
+    console.log('in changeStyle');
+
+    //toggle red/yellow color classes 
+    $(this).parent().toggleClass('red').toggleClass('yellow');
+
+
+}
